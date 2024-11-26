3c9f0ca9aa41f71c617e9912843d4bedb8bc308a
tanmay831 <tanmayjagtap831@gmail.com>
2024-11-26 14:51:39 +0530

    FIRST COMMIT

--- Patterns in cpp/simple.cpp
+++ Patterns in cpp/simple.cpp
@@ -1,14 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n=10;
-    int num=1;
-
-    for(int i = 0; i < n ; i++)    {
-        for(int j=0; j<n;j++)
+    int n;
+    
+    cout<<"enter number of rows";
+    cin>>n;
+   
+    for(int i = 0; i <=n ; i++)    {
+       
+        for(int j=0; j<=n;j++)
         {
-            cout<<num<<"";
-            num++;
+            cout<<( ch )(j+64);
+            
+        
         }
         cout<<endl;
     }
